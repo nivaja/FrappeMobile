3a3feddf908c6ef607a349730f8a23d8be2adfd0
nivaj <nivajasingh359@gmail.com>
2023-04-29 17:20:53 +0545

    AP-2 Implemented Lazy Loading on NavBar using GetBuilder and LazyLoadIndexedStack

--- lib/crm/modules/home/home_binding.dart
+++ lib/crm/modules/home/home_binding.dart
@@ -1,10 +1,14 @@
+import 'package:frappe_mobile_custom/app/modules/list/views/list_view_view.dart';
 import 'package:frappe_mobile_custom/crm/modules/home/home_controller.dart';
 import 'package:get/get.dart';
+
+import '../../../app/generic/connectivity_controller.dart';
 
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => HomeController());
+    Get.lazyPut(()=>ConnectivityController());
   }
 }
--- lib/crm/modules/home/home_controller.dart
+++ lib/crm/modules/home/home_controller.dart
@@ -3,66 +3,24 @@ import 'package:frappe_mobile_custom/app/modules/list/views/list_view_view.dart'
 import 'package:frappe_mobile_custom/app/modules/login/views/login_view.dart';
 import 'package:frappe_mobile_custom/crm/modules/setting/setting_view.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../../../app/modules/list/bindings/list_view_binding.dart';
 import '../../../app/modules/login/bindings/login_binding.dart';
 
 class HomeController extends GetxController {
-  static HomeController get to => Get.find();
-
-
-  var currentIndex = 0.obs;
-
-  final pages = <String>['/pos-invoice', '/payment', '/customer','/lead','/login'];
-
+  List<Widget> screens = <Widget>[
+    DocListView(docType: 'Sales Invoice'),
+    DocListView(docType: 'Payment Entry',hiddenValues: const {'paid_amount':500},),
+    DocListView(docType: 'Customer'),
+    DocListView(docType: 'Lead'),
+    const SettingView()
+  ];
+  var currentIndex = 0;
 
   void changePage(int index) {
-    currentIndex.value = index;
-    Get.offAndToNamed(pages[index], id: 1);
+    currentIndex = index;
+    update();
   }
 
-  Route? onGenerateRoute(RouteSettings settings){
-    if (settings.name == '/pos-invoice') {
-      String docType ='Sales Invoice';
-      return GetPageRoute(
-        settings: settings,
-        page: () => DocListView(docType: docType),
-        binding: ListViewBinding(docType: docType),
-      );
-    }
-
-    if (settings.name == '/payment') {
-      String docType ='Payment Entry';
-      return GetPageRoute(
-        settings: settings,
-        page: () => DocListView(docType: docType,hiddenValues: const {'paid_amount':500},),
-        binding: ListViewBinding(docType: docType),
-      );
-    }
-
-    if (settings.name == '/customer') {
-      String docType ='Customer';
-      return GetPageRoute(
-        settings: settings,
-        page: () => DocListView(docType: docType),
-        binding: ListViewBinding(docType: docType),
-      );
-    }
-    if (settings.name == '/lead') {
-      String docType ='Lead';
-      return GetPageRoute(
-        settings: settings,
-        page: () => DocListView(docType: docType),
-        binding: ListViewBinding(docType: docType),
-      );
-    }
-    if (settings.name == '/login') {
-      return GetPageRoute(
-        settings: settings,
-        page: () => const SettingView(),
-      );
-    }
-
-    return null;
-  }
 }
